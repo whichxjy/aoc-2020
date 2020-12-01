5c5ef696d8864204c98bd1052c34ab930ee7ab4d
whichxjy <whichxjy@gmail.com>
2020-12-02 02:34:59 +0800

    Update day 1

--- day-1/src/main.rs
+++ day-1/src/main.rs
@@ -1,25 +1,31 @@
 use std::collections::HashMap;
 use std::fs;
 
-fn main() {
-    let contents = fs::read_to_string("input.txt").expect("Fail to read input file");
-
+fn solve_part_one(entries: &[u32], target: u32) {
     // (2020 - entry) -> entry
     let mut num_map = HashMap::new();
 
-    for entry in contents
-        .split_whitespace()
-        .map(|e| e.parse::<u32>().unwrap())
-    {
-        match num_map.get(&entry) {
+    for entry in entries {
+        match num_map.get(entry) {
             Some(match_entry) => {
                 println!("Two entries: ({}, {})", entry, match_entry);
                 println!("Answer: {}", entry * match_entry);
                 break;
             }
             None => {
-                num_map.insert(2020 - entry, entry);
+                num_map.insert(target - entry, *entry);
             }
         }
     }
+}
+
+fn main() {
+    let contents = fs::read_to_string("input.txt").expect("Fail to read input file");
+    let target = 2020;
+    let entries = contents
+        .split_whitespace()
+        .map(|e| e.parse::<u32>().unwrap())
+        .collect::<Vec<u32>>();
+
+    solve_part_one(&entries, target);
 }
