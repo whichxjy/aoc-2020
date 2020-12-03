8eb2c8ba6fea4a9dd06c9c462351f84e0ef6e5ea
whichxjy <whichxjy@gmail.com>
2020-12-03 09:01:42 +0800

    Update day 2

--- day-02/src/main.rs
+++ day-02/src/main.rs
@@ -1,3 +1,10 @@
+use std::fs::File;
+use std::io::{BufRead, BufReader};
+
 fn main() {
-    println!("Hello, world!");
+    let file = BufReader::new(File::open("input.txt").unwrap());
+
+    for line in file.lines() {
+        println!("-> {}", line.unwrap());
+    }
 }
