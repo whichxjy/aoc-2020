74bac27f66a7d7c6167165a7c4cecdbce3ce299b
whichxjy <whichxjy@gmail.com>
2020-12-07 09:39:48 +0800

    Update day 6

--- day-06/src/main.rs
+++ day-06/src/main.rs
@@ -1,3 +1,23 @@
+use std::collections::HashSet;
+use std::fs;
+
 fn main() {
-    println!("Hello, world!");
+    let contents = fs::read_to_string("input.txt").expect("Fail to read input file");
+    let groups = contents.split("\n\n").collect::<Vec<&str>>();
+
+    let mut yes_sum = 0;
+
+    for group in groups {
+        let mut st = HashSet::new();
+
+        for people in group.split_whitespace() {
+            for ch in people.chars() {
+                st.insert(ch);
+            }
+        }
+
+        yes_sum += st.len();
+    }
+
+    println!("Answer: {}", yes_sum);
 }
