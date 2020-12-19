204f94abb424feaad46d5e6f990cfd9a8c53d642
whichxjy <whichxjy@gmail.com>
2020-12-19 21:24:16 +0800

    Update day 7

--- day-07/src/main.rs
+++ day-07/src/main.rs
@@ -1,3 +1,47 @@
+use std::collections::HashMap;
+use std::fs;
+
+fn parse_item(item: &str) -> (String, u32) {
+    let item = item.trim_end_matches(" bags");
+    let pieces = item.splitn(2, ' ').collect::<Vec<&str>>();
+
+    let left_piece = pieces[0];
+    let right_piece = pieces[1];
+
+    let count = left_piece.parse::<u32>().unwrap();
+    let color = right_piece.to_owned();
+
+    (color, count)
+}
+
+fn process_lines(lines: &[&str]) -> HashMap<String, std::vec::Vec<(String, u32)>> {
+    let mut color_mp = HashMap::new();
+
+    for line in lines.iter().map(|l| l.trim_end_matches('.')) {
+        let parts = line.splitn(2, " contain ").collect::<Vec<&str>>();
+
+        let left_part = parts[0];
+        let key_color = left_part.trim_end_matches(" bags").to_owned();
+
+        let right_part = parts[1];
+        let items = match right_part {
+            "no other bags" => Vec::new(),
+            _ => right_part
+                .split(", ")
+                .map(parse_item)
+                .collect::<Vec<(String, u32)>>(),
+        };
+
+        color_mp.insert(key_color, items);
+    }
+
+    color_mp
+}
+
 fn main() {
-    println!("Hello, world!");
+    let contents = fs::read_to_string("input.txt").expect("Fail to read input file");
+    let lines = contents.trim().split('\n').collect::<Vec<&str>>();
+
+    let color_mp = process_lines(&lines);
+    println!("{:#?}", color_mp);
 }
