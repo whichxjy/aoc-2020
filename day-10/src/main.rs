d4abe7341f9bd424a6252581e71a10f71b50641e
whichxjy <whichxjy@gmail.com>
2020-12-22 21:15:24 +0800

    Update day 10

--- day-10/src/main.rs
+++ day-10/src/main.rs
@@ -1,3 +1,39 @@
+fn solve_part_one(nums: &[u32]) -> u32 {
+    let mut nums = nums.to_vec();
+    nums.sort_unstable();
+
+    let mut one_jolt_count = 0;
+    let mut three_jolts_count = 1;
+
+    let mut last_rating = 0;
+
+    for num in nums {
+        let diff = num - last_rating;
+        last_rating = num;
+
+        if diff < 1 || diff > 3 {
+            continue;
+        }
+
+        if diff == 1 {
+            one_jolt_count += 1;
+        }
+
+        if diff == 3 {
+            three_jolts_count += 1;
+        }
+    }
+
+    one_jolt_count * three_jolts_count
+}
+
 fn main() {
-    println!("Hello, world!");
+    let content = include_str!("../input.txt");
+    let nums = content
+        .trim()
+        .split('\n')
+        .map(|l| l.parse::<u32>().unwrap())
+        .collect::<Vec<u32>>();
+
+    assert_eq!(solve_part_one(&nums), 1904);
 }
