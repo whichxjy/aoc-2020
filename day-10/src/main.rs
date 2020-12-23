cefe4d43e989bbb501c5c74eb77acb0c5310754b
whichxjy <whichxjy@gmail.com>
2020-12-23 11:37:25 +0800

    Update day 10

--- day-10/src/main.rs
+++ day-10/src/main.rs
@@ -1,3 +1,5 @@
+use std::collections::HashSet;
+
 fn solve_part_one(nums: &[u32]) -> u32 {
     let mut nums = nums.to_vec();
     nums.sort_unstable();
@@ -27,6 +29,35 @@ fn solve_part_one(nums: &[u32]) -> u32 {
     one_jolt_count * three_jolts_count
 }
 
+fn solve_part_two(nums: &[u32]) -> u32 {
+    fn find_ways(nums: &HashSet<&u32>) -> u32 {
+        fn find_ways_iter(curr: u32, nums: &HashSet<&u32>, target: u32, count: &mut u32) {
+            if curr == target {
+                *count += 1;
+                return;
+            }
+
+            if curr != 0 && !nums.contains(&curr) {
+                return;
+            }
+
+            for i in 1..=3 {
+                find_ways_iter(curr + i, nums, target, count);
+            }
+        }
+
+        let target = **nums.iter().max().unwrap();
+
+        let mut count = 0;
+        find_ways_iter(0, &nums, target, &mut count);
+
+        count
+    }
+
+    let nums = nums.iter().collect::<HashSet<&u32>>();
+    find_ways(&nums)
+}
+
 fn main() {
     let content = include_str!("../input.txt");
     let nums = content
@@ -36,4 +67,5 @@ fn main() {
         .collect::<Vec<u32>>();
 
     assert_eq!(solve_part_one(&nums), 1904);
+    assert_eq!(solve_part_two(&nums), 0);
 }
