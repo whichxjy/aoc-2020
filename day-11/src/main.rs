8fb9512d8dfd2f29eed7b3f913b821bf6c4a1619
whichxjy <whichxjy@gmail.com>
2020-12-23 18:39:26 +0800

    Update day 11

--- day-11/src/main.rs
+++ day-11/src/main.rs
@@ -124,10 +124,115 @@ fn solve_part_one(layout: &Layout) -> usize {
         .count()
 }
 
+fn solve_part_two(layout: &Layout) -> usize {
+    fn get_visible_seats(layout: &Layout, row: usize, col: usize) -> Vec<(usize, usize)> {
+        let offsets: Vec<(i32, i32)> = vec![
+            (-1, -1),
+            (-1, 0),
+            (-1, 1),
+            (0, -1),
+            (0, 1),
+            (1, -1),
+            (1, 0),
+            (1, 1),
+        ];
+
+        let mut seats = Vec::new();
+
+        for (row_offset, col_offset) in offsets {
+            let mut curr_row = row as i32;
+            let mut curr_col = col as i32;
+
+            loop {
+                curr_row += row_offset;
+                curr_col += col_offset;
+
+                if curr_row < 0
+                    || curr_col < 0
+                    || curr_row >= layout.row_num as i32
+                    || curr_col >= layout.col_num as i32
+                {
+                    break;
+                }
+
+                match layout.seat_map[curr_row as usize][curr_col as usize] {
+                    SeatKind::Floor => continue,
+                    _ => {
+                        seats.push((curr_row as usize, curr_col as usize));
+                        break;
+                    }
+                }
+            }
+        }
+
+        seats
+    }
+
+    fn map_seat(layout: &Layout, row: usize, col: usize) -> SeatKind {
+        match layout.seat_map[row][col] {
+            SeatKind::Floor => SeatKind::Floor,
+            SeatKind::Empty => match get_visible_seats(layout, row, col)
+                .iter()
+                .map(|(next_row, next_col)| layout.seat_map[*next_row][*next_col].clone())
+                .all(|seat_kind| seat_kind != SeatKind::Occupied)
+            {
+                true => SeatKind::Occupied,
+                false => SeatKind::Empty,
+            },
+            SeatKind::Occupied => {
+                let occupied_adjs_count = get_visible_seats(layout, row, col)
+                    .iter()
+                    .map(|(next_row, next_col)| layout.seat_map[*next_row][*next_col].clone())
+                    .filter(|seat_kind| *seat_kind == SeatKind::Occupied)
+                    .count();
+
+                if occupied_adjs_count >= 5 {
+                    SeatKind::Empty
+                } else {
+                    SeatKind::Occupied
+                }
+            }
+        }
+    }
+
+    let mut layout = layout.to_owned();
+
+    loop {
+        let mut is_changed = false;
+
+        let mut new_seat_map = layout.seat_map.to_owned();
+        for (row, seat_line) in new_seat_map.iter_mut().enumerate() {
+            for (col, seat) in seat_line.iter_mut().enumerate() {
+                let new_seat = map_seat(&layout, row, col);
+
+                if new_seat != layout.seat_map[row][col] {
+                    is_changed = true;
+                }
+
+                *seat = new_seat;
+            }
+        }
+
+        layout.seat_map = new_seat_map;
+
+        if !is_changed {
+            break;
+        }
+    }
+
+    layout
+        .seat_map
+        .iter()
+        .flatten()
+        .filter(|&seat_kind| *seat_kind == SeatKind::Occupied)
+        .count()
+}
+
 fn main() {
     let content = include_str!("../input.txt");
     let lines = content.lines().collect::<Vec<&str>>();
     let layout = parse_layout(&lines);
 
     assert_eq!(solve_part_one(&layout), 2324);
+    assert_eq!(solve_part_two(&layout), 2068);
 }
